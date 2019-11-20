d704f7cbc37e1652d96209a74c76f3578a3413b1
Dima Kokarovtsev <dima.kokarovtsev@gmail.com>
2019-11-19 19:22:22 -0500

    Fixed audio playback in chromium. Cleaned up sound service code.

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -5,8 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class SoundService {
 
-  constructor() { }
-
+  audioCtx
   notes = [
     261.63,
     293.66,
@@ -26,6 +25,8 @@ export class SoundService {
   counter = 0
   chord = 0
 
+  constructor() { this.audioCtx = new AudioContext() }
+
   createAmplifier(audio, startValue, duration) {
     const amplifier = audio.createGain();
     this.rampDown(audio, amplifier.gain, startValue, duration);
@@ -37,121 +38,34 @@ export class SoundService {
     value.exponentialRampToValueAtTime(0.01, audio.currentTime + duration);
   };
 
-  async special() {
-    const audioCtx = new AudioContext()
-    const root = audioCtx.createOscillator()
-    const third = audioCtx.createOscillator()
-    const fifth = audioCtx.createOscillator()
-    root.type = "triangle"
-    third.type = "triangle"
-    fifth.type = "triangle"
+  chain(soundNodes) {
+    for (let i = 0; i < soundNodes.length - 1; i++) {
+      soundNodes[i].connect(soundNodes[i + 1]);
+    }
+  };
 
-    root.frequency.value = this.notes[0]
-    root.start()
-    root.stop(audioCtx.currentTime + 0.5);
-
-    third.frequency.value = this.notes[2]
-    third.start(0.4)
-    third.stop(audioCtx.currentTime + 0.9);
-
-    fifth.frequency.value = this.notes[4]
-    fifth.start(0.2)
-    fifth.stop(audioCtx.currentTime + 0.7);
-
-    this.chain([
-      root,
-      this.createAmplifier(audioCtx, 0.3, 0.5),
-      audioCtx.destination
-    ])
-    this.chain([
-      third,
-      this.createAmplifier(audioCtx, 0.3, 0.5),
-      audioCtx.destination
-    ])
-    this.chain([
-      fifth,
-      this.createAmplifier(audioCtx, 0.3, 0.5),
-      audioCtx.destination
-    ])
-  }
-
-  async bad() {
-    const audioCtx = new AudioContext()
-    const root = audioCtx.createOscillator()
-    const fifth = audioCtx.createOscillator()
-    root.type = "square"
-    fifth.type = "square"
-
-    root.frequency.value = this.notes[0]
-    root.start()
-    root.stop(audioCtx.currentTime + 0.5);
-
-    fifth.frequency.value = 369.99
-    fifth.start()
-    fifth.stop(audioCtx.currentTime + 0.5);
-
-    this.chain([
-      root,
-      this.createAmplifier(audioCtx, 0.3, 0.5),
-      audioCtx.destination
-    ])
-    this.chain([
-      fifth,
-      this.createAmplifier(audioCtx, 0.3, 0.5),
-      audioCtx.destination
-    ])
-  }
-
-  async sing(value, instrument = 1) {
-    const audioCtx = new AudioContext()
-    const sine = audioCtx.createOscillator()
-    const square = audioCtx.createOscillator()
-    square.type = "square"
-
+  sing(txValue, duration = 1) {
+    const sine = this.audioCtx.createOscillator()
     let note
-    if (value < 0.1) {
-      note = 0
-    }
-    else if (value < 0.5) {
-      note = 1
-    }
-    else if (value < 1) {
-      note = 2
-    }
-    else {
-      note = 3
-    }
+    if (txValue < 0.1) { note = 0 }
+    else if (txValue < 0.5) { note = 1 }
+    else if (txValue < 1) { note = 2 }
+    else { note = 3 }
 
     note = note * 2 + this.progression[this.chord]
     if (note > 7) {
       note -= 8
     }
 
-    let duration = 1
-    if (this.counter == 0){
-      duration = 3
-    }
-
     sine.frequency.value = this.notes[note]
     sine.start()
-    sine.stop(audioCtx.currentTime + duration);
-    square.frequency.value = this.notes[note]
-    square.start()
-    square.stop(audioCtx.currentTime + duration);
+    sine.stop(this.audioCtx.currentTime + duration);
 
     this.chain([
       sine,
-      this.createAmplifier(audioCtx, 0.5, duration),
-      audioCtx.destination
+      this.createAmplifier(this.audioCtx, 0.4, duration),
+      this.audioCtx.destination
     ])
-
-    if (instrument == 1) {
-      this.chain([
-        square,
-        this.createAmplifier(audioCtx, 0.01, duration),
-        audioCtx.destination
-      ])
-    }
 
     this.counter++;
     if (this.counter == 4) {
@@ -160,9 +74,7 @@ export class SoundService {
     }
     if (this.chord > 3) this.chord = 0;
   }
-  chain(soundNodes) {
-    for (let i = 0; i < soundNodes.length - 1; i++) {
-      soundNodes[i].connect(soundNodes[i + 1]);
-    }
-  };
 }
+
+
+
